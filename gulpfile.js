5a63fc6c024693045f47e9915ecb73111a6c6303
Arden de Raaij <a.de.raaij@gmail.com>
2015-05-16 12:14:31 +0200

    comments in gulpfile, cleanups

--- gulpfile.js
+++ gulpfile.js
@@ -36,78 +36,118 @@ var onError = function (err) {
   this.emit('end');
 };
 
+// Install bower components in specified folder
 gulp.task('bower', function() {
   return bower()
     .pipe(gulp.dest('./bower_components'));
 });
 
-// Templates
+// Execute Jade Templates
 gulp.task('templates', function() {
   return gulp.src(src_path+'**/*.jade')
-    .pipe(plumber({
-      errorHandler: onError
-    }))
+    // Catch errors
+    .pipe(plumber({errorHandler: onError}))
+
+    // Only build changed files
     .pipe(changed(build_path, {extension: '.html'}))
+
     .pipe(gulpif(global.isWatching, cached('jade')))
+
+    // Watch partials for change
     .pipe(jadeInheritance({basedir: src_path}))
+
+    // Ignore build of files starting with _
     .pipe(filter(function (file) {
       return !/\/_/.test(file.path) && !/^_/.test(file.relative);
     }))
+
+    // Output Jade
     .pipe(jade({pretty: true}))
+
+    // Catch errors
     .pipe(plumber({errorHandler: onError}))
+
+    // Distribute to build path
     .pipe(gulp.dest(build_path))
+
+    // Show notification
     .pipe(notify({ message: 'templates task complete' }));
 });
 
 // Styles
-gulp.task('styles', function() {
+gulp.task('styles', ['bower'], function() {
   return gulp.src(src_path+'assets/sass/screen.scss')
-    .pipe(plumber({
-      errorHandler: onError
-    }))
+
+    // Catch errors
+    .pipe(plumber({errorHandler: onError}))
+
+    // Specify output style
     .pipe(sass({outputStyle: 'nested'}))
+
+    // Autoprefixer
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+
+    // Distribute to build
     .pipe(gulp.dest(build_path+'assets/css/'))
-    .pipe(gulp.dest(theme_path + '/assets/css/'))
+
+    // Add a .min version
     .pipe(rename({ suffix: '.min' }))
+
+    // Minify .min version
     .pipe(minifycss())
+
+    // Distribute to build path
     .pipe(gulp.dest(build_path+'assets/css/'))
-    .pipe(gulp.dest(theme_path + '/assets/css/'))
+
+    // Show notification
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Scripts
-gulp.task('scripts', function() {
+gulp.task('scripts', ['bower'], function() {
   return gulp.src([
-      src_path+'assets/js/vendor/moment.js',
-      src_path+'assets/js/vendor/pikaday.js',
       src_path+'assets/js/vendor/*.js',
       src_path+'assets/js/partials/*.js',
       src_path+'assets/js/*.js'
     ])
-    .pipe(plumber({
-      errorHandler: onError
-    }))
+
+    // Catch Errors
+    .pipe(plumber({errorHandler: onError}))
+
+    // Concatinate in one file
     .pipe(concat('script.js'))
+
+    // Distribute to build
     .pipe(gulp.dest(build_path + 'assets/js/'))
-    .pipe(gulp.dest(theme_path + 'assets/js/'))
+
+    // Add a .min version
     .pipe(rename({ suffix: '.min' }))
+
+    // Minify with jsUglify
     .pipe(uglify())
+
+    // Distribute to build
     .pipe(gulp.dest(build_path + 'assets/js/'))
-    .pipe(gulp.dest(theme_path + 'assets/js/'))
+
+    // Show notifcation
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Images
 gulp.task('images', function() {
   return gulp.src(src_path+'assets/img/**/**/*')
+
+    // Image optimization
     .pipe(cache(imagemin({
       optimizationLevel: 3,
       progressive: true,
       interlaced: true
     })))
+
+    // Distribute to build path
     .pipe(gulp.dest(build_path+'assets/img/'))
-    .pipe(gulp.dest(theme_path + 'assets/img/'))
+
+    // Show notification
     .pipe(notify({ message: 'Images task complete' }));
 });
 
@@ -116,21 +156,20 @@ gulp.task('clear', function (done) {
   return cache.clearAll(done);
 });
 
-// Clean everything
+// Clean out build
 gulp.task('clean', function(cb) {
-  del([build_path+'assets/css', build_path+'assets/js', build_path+'assets/img', build_path], cb);
+  del(build_path, cb);
 });
 
-// Copy font files
-gulp.task('copyfonts',['clean'], function() {
+// Copy font files, clean before copy
+gulp.task('copyfonts', ['clean'], function() {
   gulp.src(src_path+'assets/fonts/**/*')
-  .pipe(gulp.dest(build_path+'assets/fonts'))
-  .pipe(gulp.dest(theme_path+'assets/fonts'));
+  .pipe(gulp.dest(build_path+'assets/fonts'));
 });
 
-// Default task
+// Run clean task before default task
 gulp.task('default', ['clean'], function() {
-  gulp.start('clean', 'templates', 'styles', 'scripts', 'images', 'copyfonts');
+  gulp.start('bower', 'templates', 'styles', 'scripts', 'images', 'copyfonts');
 });
 
 // Set watch task for template caching and
@@ -147,19 +186,19 @@ gulp.task('watch', ['setWatch', 'templates'], function() {
   // Watch .jade files
   gulp.watch(src_path+'**/*.jade', ['templates']);
 
-  // Images
+  // Watch image folder
   gulp.watch(src_path+'assets/img/**/*', ['images']);
 
   // Watch .js files
   gulp.watch(src_path+'assets/js/**/*.js', ['scripts']);
 
-  // Copy stuff
+  // Copy fonts
   gulp.watch(src_path+'assets/fonts/**/*', ['copyfonts']);
 
   // Create LiveReload server
   livereload.listen();
 
-  // Watch any files in build/, reload on change
+  // Watch any files in build directory, reload on change
   gulp.watch([build_path+'/**/**/*']).on('change', livereload.changed);
 
 });
