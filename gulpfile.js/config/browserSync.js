ac982ae94f26d46a46e7b938e6ae21dc91f43e3d
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-11 15:29:47 +0200

    browsersync updates

--- gulpfile.js/config/browserSync.js
+++ gulpfile.js/config/browserSync.js
@@ -3,6 +3,5 @@ var config = require('./')
 module.exports = {
   server: {
     baseDir: config.buildPath
-  },
-  files: ['**/*']
+  }
 }
--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -8,14 +8,11 @@ var
   images                  = require('../config/images'),
   plugins                 = require('gulp-load-plugins')();
 
-gulp.task('watch', ['setWatch', 'templates'], function() {
+gulp.task('watch', ['browserSync','setWatch', 'templates'], function() {
 
   // Watch all the things with the gulp-watch plugins
   plugins.watch(templates.source, function() { gulp.start('jade-watch'); });
   plugins.watch(styles.base, function() { gulp.start('styles'); });
   plugins.watch(scripts.source, function() { gulp.start('scripts'); });
   plugins.watch(images.source, function() { gulp.start('images'); });
-
-  // Start browsersync server
-  browserSync({server: config.buildPath});
 });
