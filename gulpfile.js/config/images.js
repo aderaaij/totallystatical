c92bdc34c2c1afebafeed646c074818b45c7ff4f
Arden de Raaij <a.de.raaij@gmail.com>
2015-08-13 16:52:00 +0200

    ignore sprite.symbol.svg in image task

--- gulpfile.js/config/images.js
+++ gulpfile.js/config/images.js
@@ -1,7 +1,10 @@
 var config = require('./')
 
 module.exports = {
-  source: config.assetsPath+'img/**/*',
+  source: [
+  	config.assetsPath+'img/**/*',
+  	'!'+config.assetsPath+'img/symbol/svg/sprite.symbol.svg'
+  ],
   dest: config.buildPath+'assets/img/',
   imagemin: {
     optimizationLevel: 3,
