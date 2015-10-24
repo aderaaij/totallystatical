808372e9fdff20b7ef60d4d10124281324120d49
Arden de Raaij <a.de.raaij@gmail.com>
2015-10-24 20:12:16 +0200

    production build task

---
+++ gulpfile.js/tasks/buildProduction.js
@@ -0,0 +1,22 @@
+var
+  gulp                = require('gulp'),
+  plugins             = require('gulp-load-plugins')();
+
+gulp.task('build:production', function(cb) {
+  plugins.sequence(
+    'clean',
+    [
+      'bower'
+    ],
+    [
+      'images',
+      'scripts:standalone'
+    ],
+    [
+      'styles:production',
+      'scripts',
+      'templates'
+    ],
+    cb
+  );
+});
