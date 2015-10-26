4627182d78f324c9fbbb5fc071da4928750ac6e4
Arden de Raaij <a.de.raaij@gmail.com>
2015-10-26 11:24:35 +0100

    [refactor] split production styles task into seperate file

---
+++ gulpfile.js/tasks/stylesProduction.js
@@ -0,0 +1,21 @@
+var browserSync         = require('browser-sync')
+var gulp                = require('gulp')
+var plugins             = require('gulp-load-plugins')()
+var config              = require('../config/styles')
+var errorHandler        = require('../lib/errorHandler')
+
+var stylesProductionTask = function(cb) {
+  return gulp.src(config.source)
+
+    .pipe(plugins.sass(config.settings))
+
+    .on('error', errorHandler)
+
+    .pipe(plugins.autoprefixer(config.autoprefixer))
+
+    .pipe(plugins.minifyCss())
+
+    .pipe(gulp.dest(config.dest))
+}
+gulp.task('styles:production', stylesProductionTask)
+module.exports = stylesProductionTask
