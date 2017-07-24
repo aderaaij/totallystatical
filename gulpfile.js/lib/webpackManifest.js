b738791941bdf763384ecbd931a8d966b5b882cf
Arden de Raaij <a.de.raaij@gmail.com>
2017-07-24 13:46:38 +0100

    [fix] enhanced-resolve error?

--- gulpfile.js/lib/webpackManifest.js
+++ gulpfile.js/lib/webpackManifest.js
@@ -6,8 +6,8 @@ module.exports = function (publicPath, dest, filename) {
 
     return function () {
         this.plugin('done', (stats) => {
-            var stats = stats.toJson();
-            const chunks = stats.assetsByChunkName;
+            const statsJSON = stats.toJson();
+            const chunks = statsJSON.assetsByChunkName;
             const manifest = {};
 
             for (const key in chunks) {
@@ -16,9 +16,9 @@ module.exports = function (publicPath, dest, filename) {
             }
 
             fs.writeFileSync(
-        path.join(process.cwd(), dest, filename),
-        JSON.stringify(manifest),
-      );
+                path.join(process.cwd(), dest, filename),
+                JSON.stringify(manifest),
+            );
         });
     };
 };
